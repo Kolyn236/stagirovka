2c6114e30e43cf0975902920030ec0d561c1be00
kolyn236 <kolyn236@mail.ru>
2016-08-18 22:56:46 +0400

    Изменения

--- blog/views/articles.php
+++ blog/views/articles.php
@@ -2,16 +2,15 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Мой первый блог</title>
+        <title>Это первый блог</title>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
-    <body>
+      <body bgcolor="#000" background="/pictures/background.jpg">
         <div>
             
             <div class="container">
-                <h1>Общая страница</h1>
-                <a href="admin" >Панель администратора</a>
+                <h1 align="center">Интересная информация</h1>
                 <h3><?php foreach($articles as $a): ?></h3>
                     <div class="article">
                         <h3><a href="article.php?id=<?=$a['id']?>"><?=$a['title']?></a></h3>
@@ -22,9 +21,22 @@
             </div>
             
             <footer>
-                <p>Мой первый блог
-                <br>Copyrihtt &copy; 2016</p>
+               
+                 <address>
+                              <strong>ДычковЪ, Inc.</strong><br>
+                              Ульяновская обл. г.Ульяновск<br>
+                              Радищева 10А<br>
+                              <abbr title="Phone">P:</abbr> 8 987-637-0893
+                            </address>
+
+                    <address>
+                      <strong>Мой адрес электронной почты:</strong><br>
+                      <a href="mailto:#">kolyn236@mail.ru</a>
+                    </address>
+                 
             </footer>
+            <h5 align="right"><a href="admin" >Панель администратора</a></h5>
+                
         </div>
     </body>
 
